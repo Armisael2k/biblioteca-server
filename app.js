d64588883c869382258ad4f4754ee0c91eee566a
Armisael2k <chuylopez755@gmail.com>
2022-09-25 18:00:28 -0700

    implementacion login

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const http = require('http');
+const crypto = require('crypto');
 const app = express();
 const server = http.createServer(app);
 const con = require('./db');
+app.use(express.json());
 app.set('port', 891);
 
-app.get('/api/vacantes', function(req, res) {
+app.get('/api/vacantes', (req, res) => {
     con.query(
         'SELECT v.*, u.nombre publicador_nombre\
         FROM vacantes v\
         INNER JOIN usuarios u ON v.id_usuario = u.id',
         (err, result) => {
-            console.log(result);
             if (err) { console.error(err.sqlMessage); return res.send({ success: 0, msg: err.sqlMessage }) }
             res.send({ success: 1, result: result });
+        }
+    );
+});
+
+app.post('/api/acceder', async (req, res) => {
+    let { user, password } = req.body;
+    password = crypto.createHash('sha256').update(password).digest('hex');
+    con.query(
+        'SELECT id, nombre, usuario FROM usuarios WHERE usuario=? AND contrasena=?',
+        [user, password],
+        (err, result) => {
+            if (err) { console.error(err.sqlMessage); return res.send({ success: 0, msg: err.sqlMessage }) }
+            res.send({ success: 1, result: result[0] });
         }
     );
 });
